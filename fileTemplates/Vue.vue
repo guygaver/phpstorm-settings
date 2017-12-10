feb137ad9635eb603c458b30ef35e42cbe58c774
Guy G <guy.gavergun@gmail.com>
2017-12-10 13:03:27 -0500

    PS-2017.2.4 <Guy@Guys-MacBook-Pro-3.local Create markdown.xml, PHP Class.php, IntelliLang.xml, monkoose.icls, .DS_Store, dark.icls.icls, project.default.xml, diff.xml, Default copy.xml, Laravel.xml, vagrant.xml, Default.xml, yka_0.icl, file.template.settings.xml, ignore.xml, find.xml, code.style.schemes.xml, Zen HTML.xml, github_settings.xml, Tomorrow Night.icls, JavaScript File.js, Guys Keys.xml, Dark Visual Studio.icls, Obsidian.icls, hg.xml, bootstrap.xml, AciD_0.icls, Default.xml, PHP.xml, PHTML.phtml, JavaScript.xml, Pastell_4.icls.icls, ObsidiMod.icls, editor.codeinsight.xml, keymap.xml, editor.xml, Blade༏ext༏.blade.php, applicationLibraries.xml, debugger.xml, Laravel.xml, PHP File.php, baseRefactoring.xml, tibau_0.icls.icls, ide.general.xml, Son of Darcula.icls, user.xml, git.xml, other.xml, My Style.xml, Bulma.xml, terminal.xml, ui.lnf.xml, Bootstrap.xml, filetypes.xml, php.xml, javaeeExternalResources.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, Dark Room.icls, templates.xml, PHPUnitTestCase.php, Vue.vue, PHPUnit Test.php, laf.xml, JSON File.json, web-browsers.xml, moldcraft_1.icls, material_theme.xml, PHP Interface.php, Solarizeddd.icls, databaseSettings.xml, Spacegray_1.icls.icls, RecognEyes.icls

---
+++ fileTemplates/Vue.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: '${COMPONENTNAME}',
+        
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
